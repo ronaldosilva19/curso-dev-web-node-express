58e5353ef41292d9acef7d19f70f6a60b023bf5d
iceberg20 <italoberg@yahoo.com.br>
2020-01-20 20:42:39 -0300

    refactor

--- node_modulos/main.js
+++
@@ -1,5 +0,0 @@
-var somafunc = require("./somar");
-var divfunc = require("./dividir");
-
-console.log(somafunc(10,20));
-console.log(divfunc(10,2));
--- node_modulos/somar.js
+++
@@ -1,5 +0,0 @@
-var somar = function soma(a,b){
-	return a+b;
-}
-
-module.exports = somar;
